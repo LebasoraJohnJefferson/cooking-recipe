6b0ff35a3f22e250af4d4c38d2e996758a8b4a31
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2022-06-11 14:45:30 +0800

    adding the nextjs files

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import '../styles/globals.css'
+import Layout from '../components/Layout'
+import Head from 'next/head'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+    {/* meta tags for SEO */}
+    <Head>
+      <meta name="description" content="The web offer free ingredients and step-by-step cooking tutorial." />
+      <meta name="keywords" content="Recipe, Cooking, ingredients list " />
+      <meta name="author" content="Lebasora, John Jefferson ; Paranas , Christian ; Luigi, Loreno" />
+      <meta name="robots" content="index, follow" />
+    </Head>
+    <Layout>
+      {/* navigation header */}
+      <header>
+        <div>
+          <h1>Learn to Cook</h1>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </Layout>
+    </>
+  )
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const index = () => {
+  return (
+    <div>
+        <Head>
+            <title>Cooking Recipe</title>
+        </Head>
+        index
+    </div>
+  )
+}
+
+export default index
