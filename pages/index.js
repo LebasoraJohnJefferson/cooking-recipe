15de792eeeafae25e1edcfc1005aa787f5fe5954
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2022-06-13 11:37:26 +0800

    initail api calls

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import {server} from '../config'
+// import {server} from '../config'
 import Head from 'next/head'
 import { Categories } from '../components/Categories';
 
@@ -18,8 +18,9 @@ const index = ({categories}) => {
 export default index
 
 export const getStaticProps = async ()=>{
-  const res = await fetch(`${server}/api/categories`)
-  const categories = await res.json()
+  // const res = await fetch(`${server}/api/categories`)
+  const res = await fetch(`https://themealdb.com/api/json/v1/1/categories.php`)
+  const categories = await res.json() 
   return{
     props:{categories}
   }
