14b11fbd5358ff79bfb939ac451c5981f880c12c
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2022-06-13 11:57:27 +0800

    excellent length of meta desciption and meta title

--- pages/api/categories/index.js
+++ pages/api/categories/index.js
@@ -3,3 +3,5 @@ export default async function handler(req,res){
     const categories = await data.json()
     res.status(200).json(categories)
 }
+
+// error ocurred in productions
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-// import {server} from '../config'
 import Head from 'next/head'
 import { Categories } from '../components/Categories';
 
@@ -18,7 +17,6 @@ const index = ({categories}) => {
 export default index
 
 export const getStaticProps = async ()=>{
-  // const res = await fetch(`${server}/api/categories`)
   const res = await fetch(`https://themealdb.com/api/json/v1/1/categories.php`)
   const categories = await res.json() 
   return{
