5fd27ab18916b38cd27090b095084c51ac578a47
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2022-06-13 17:46:56 +0800

    rewrite the address of domain

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ?  'http://localhost:3000' : 'cooking-recipe-blue.vercel.app'
+export const server = dev ?  'http://localhost:3000' : 'https://cooking-recipe-blue.vercel.app/'
