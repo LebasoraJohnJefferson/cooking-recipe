0bc0737b34ff66980346c4aff017278931c1c055
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2022-06-13 13:30:58 +0800

    substitute of api calls for now

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { server } from '../config'
+// import { server } from '../config'
 import Head from "next/head";
 import { Categories } from "../components/Categories";
 
@@ -18,7 +18,8 @@ const Home = ({ categories }) => {
 export default Home;
 
 export const getStaticProps = async ()=>{
-  const res = await fetch(`${server}/api/categories/`)
+  // const res = await fetch(`${server}/api/categories/`)
+  const res = await fetch(`https:themealdb.com/api/json/v1/1/categories.php`)
   const categories = await res.json()
   return{
     props:{categories}
