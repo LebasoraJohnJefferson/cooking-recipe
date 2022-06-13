996d850a111b8a3819217adfe631698abdef75af
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2022-06-13 13:21:07 +0800

    fix(api): server error when doing api calls

--- pages/api/categories/index.js
+++ pages/api/categories/index.js
@@ -1,6 +1,7 @@
 export default async function handler(req,res){
-    const data = await fetch(`https://themealdb.com/api/json/v1/1/categories.php`)
+    const data = await fetch(`https:themealdb.com/api/json/v1/1/categories.php`)
     const categories = await data.json()
+    console.log(categories)
     res.status(200).json(categories)
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,26 @@
-import { server } from "../config";
-import Head from "next/head";
-import { Categories } from "../components/Categories";
+import {server} from '../config/'
+import Head from 'next/head'
+import { Categories } from '../components/Categories';
 
-const index = ({ categories }) => {
-  console.log(categories);
-
+const index = ({categories}) => {
   return (
     <div>
-      <Head>
-        <title>Cooking Recipe</title>
-      </Head>
+        <Head>
+            <title>Cooking Recipe  For Everyone</title>
+        </Head>
 
-      <Categories categories={categories} />
+        <Categories categories={categories} />
+        
     </div>
-  );
-};
+  )
+}
 
-export default index;
+export default index
 
-export const getStaticProps = async () => {
-  const res = await fetch(`${server}/api/categories`);
-  const categories = await res.json();
-  return {
-    props: { categories },
-  };
-};
+export const getStaticProps = async ()=>{
+  const res = await fetch(`${server}/api/categories/`)
+  const categories = await res.json()
+  return{
+    props:{categories}
+  }
+}
