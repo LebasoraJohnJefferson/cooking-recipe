323e46c0e6deb882e81a3b83131e45f8c81c29f1
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2022-06-13 11:07:36 +0800

    initial api calls

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,25 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import Link from "next/link"
+import Image from "next/image"
 
-const Layout = ({children}) => {
+const Layout = ({children}) => {  
   return (
     <>
       {/* navigation header */}
-      <header>
+      <header >
         <div  className="bg-white drop-shadow h-max p-5 grid grid-row-1 grid-flow-col">
-          <h1>Learn to Cook</h1>
-          <div className="w-100 grid grid-row-1 grid-col-6 gap-3">
-            <input name="searchContent" className="col-start-1 col-span-4 drop-shadow rounded outline-0 indent-3" placeholder='Search ...'/>
-            <FontAwesomeIcon icon={faMagnifyingGlass} size="ms" className='col-start-5 col-span-1' />
-            <div className='col-end-7 col-span-1'>
-              menuBar
-            </div>
+          <h1 className="flex items-center justify-self-center align-item-center"><Image src="/chef.svg" height={30} width={30} alt="chef icon"/>Cooking Recipe</h1>
+          <div className='col-end-7 col-span-1'>
+            <ul className="grid grid-row-1 grid-col-3">
+              <li className='col-start-1 col-span-1'>
+                <Link href="/">Home</Link>
+              </li>
+              <li className='col-start-2 col-span-1'>
+                <Link href="https://www.themealdb.com/api.php">API</Link>
+              </li>
+              <li className='col-start-3 col-span-1'>
+                <Link href="/aboutUs">About Us</Link>
+              </li>
+            </ul>
           </div>
         </div>
       </header>
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
+import {server} from '../config'
 import Head from 'next/head'
+import { Categories } from '../components/Categories';
 
-const index = () => {
+const index = ({categories}) => {
   return (
     <div>
         <Head>
             <title>Cooking Recipe</title>
         </Head>
 
-        hello wolrd
+        <Categories categories={categories} />
         
     </div>
   )
 }
 
 export default index
+
+export const getStaticProps = async ()=>{
+  const res = await fetch(`${server}/api/categories`)
+  const categories = await res.json()
+  return{
+    props:{categories}
+  }
+}
